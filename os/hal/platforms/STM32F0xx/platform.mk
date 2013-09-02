80466a13a266ee6c585bf4471538cd81eb845b28
mobyfab <mobyfab@free.fr>
2013-09-02 15:56:24 +0200

    Missing include path.

--- os/hal/platforms/STM32F0xx/platform.mk
+++ os/hal/platforms/STM32F0xx/platform.mk
@@ -17,6 +17,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32F0xx/stm32_dma.c \
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/platforms/STM32F0xx \
               ${CHIBIOS}/os/hal/platforms/STM32 \
+              ${CHIBIOS}/os/hal/platforms/STM32/DACv1 \
               ${CHIBIOS}/os/hal/platforms/STM32/GPIOv2 \
               ${CHIBIOS}/os/hal/platforms/STM32/I2Cv2 \
               ${CHIBIOS}/os/hal/platforms/STM32/SPIv2 \
