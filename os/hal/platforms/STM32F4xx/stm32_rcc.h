a94cdb84093927caca2f698a8359b3de5502a785
mobyfab <mobyfab@free.fr>
2013-09-02 15:37:23 +0200

    Updating RCC/Registry files for DAC.

--- os/hal/platforms/STM32F4xx/stm32_rcc.h
+++ os/hal/platforms/STM32F4xx/stm32_rcc.h
@@ -332,6 +332,36 @@
 /** @} */
 
 /**
+ * @name    DAC peripheral specific RCC operations
+ * @{
+ */
+/**
+ * @brief   Enables the DAC peripheral clock.
+ *
+ * @param[in] lp        low power enable flag
+ *
+ * @api
+ */
+#define rccEnableDAC(lp) rccEnableAPB1(RCC_APB1ENR_DACEN, lp)
+
+/**
+ * @brief   Disables the DAC peripheral clock.
+ *
+ * @param[in] lp        low power enable flag
+ *
+ * @api
+ */
+#define rccDisableDAC(lp) rccDisableAPB1(RCC_APB1ENR_DACEN, lp)
+
+/**
+ * @brief   Resets the DAC peripheral.
+ *
+ * @api
+ */
+#define rccResetDAC() rccResetAPB1(RCC_APB1RSTR_DACRST)
+/** @} */
+
+/**
  * @name    DMA peripheral specific RCC operations
  * @{
  */
